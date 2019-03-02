b69d8f9a5fb197fd787855362a69e96aa0c5a2a8
phantom820 <1611821@students.wits.ac.za>
2019-03-02 14:58:08 +0200

    added button

--- app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
+++ app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
@@ -2,11 +2,12 @@ package com.example.a1611821.doctor_appointment_booking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    
 
+    Button v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
