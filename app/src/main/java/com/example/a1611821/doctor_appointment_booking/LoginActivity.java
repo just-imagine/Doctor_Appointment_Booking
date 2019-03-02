5b3ef84326c9c5a9c958676748281d8ecd505d88
JusticeShai <1385421@students.wits.ac.za>
2019-03-02 16:29:06 +0200

    Created a login homepage and a registration page,login page has username and password input fields, registration page is for creating a new account where relevant information for an account is collected such as identity number and other relevant details.
    Main contributors
    1.Justice Shai (1385421)
    2.Bafana Mbata (1629230)
    3.Tshifhiwa Mavhona(1613720)
    4.Tshepo Nkambule (1611821)
    5. Freddy Maepa(1422085)

--- app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
+++ app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.a1611821.doctor_appointment_booking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,4 +15,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void _register(View view){
+        startActivity(new Intent(getApplicationContext(), WelcomeActivity2.class));
+
+    }
+   
 }
