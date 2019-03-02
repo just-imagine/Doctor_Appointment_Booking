23b08f4102749df13d465af74bc43d87986d0487
phantom820 <1611821@students.wits.ac.za>
2019-03-02 14:54:33 +0200

    Added a textview

--- app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
+++ app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.a1611821.doctor_appointment_booking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
