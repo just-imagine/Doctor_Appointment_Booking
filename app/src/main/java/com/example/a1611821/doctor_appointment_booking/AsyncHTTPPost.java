79c9e27d987633224ba29ced49056d0869ad47a5
phantom820 <1611821@students.wits.ac.za>
2019-03-03 16:41:26 +0200

    Changed the checking of input fields for registration when all values were null the application was crashing.

---
+++ app/src/main/java/com/example/a1611821/doctor_appointment_booking/AsyncHTTPPost.java
@@ -0,0 +1,72 @@
+package com.example.a1611821.doctor_appointment_booking;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public  abstract class AsyncHTTPPost extends AsyncTask<String, String,
+        String> {
+    String address;
+    ContentValues parameters;
+    String result="";
+
+    public AsyncHTTPPost(String address, ContentValues parameters) {
+        this.address = address;
+        this.parameters = parameters;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type",
+                      "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            if (parameters.size() > 0) {
+                Uri.Builder builder = new Uri.Builder();
+                for (String s : parameters.keySet()) {
+                    builder.appendQueryParameter(s,
+                            parameters.getAsString(s));
+                }
+                String query = builder.build().getEncodedQuery();
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+            }
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            String response = br.readLine();
+            br.close();
+            result=response;
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "";
+        }
+    }
+
+    @Override
+    protected abstract void onPostExecute(String output);
+
+    public  String State (){
+        return result;
+    }
+
+}
