3988740c2048391953d72105bc95876042b2d234
rootRIDDICK <1613720@students.wits.ac.za>
2019-04-01 16:30:46 +0200

    adding unit test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Doctor_Appointment_Booking
+[![Build Status](https://travis-ci.org/just-imagine/Doctor_Appointment_Booking.svg?branch=master)](https://travis-ci.org/just-imagine/Doctor_Appointment_Booking)
+[![codecov](https://codecov.io/gh/just-imagine/Doctor_Appointment_Booking/branch/master/graph/badge.svg)](https://codecov.io/gh/just-imagine/Doctor_Appointment_Booking)
---
+++ app/src/androidTest/java/com/example/a1611821/doctor_appointment_booking/LoginActivityTest.java
@@ -0,0 +1,74 @@
+package com.example.a1611821.doctor_appointment_booking;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+    @Rule
+    public ActivityTestRule<LoginActivity>mActivityTestRule= new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    private LoginActivity mActivity= null;
+    Instrumentation.ActivityMonitor monitor= getInstrumentation().addMonitor(HomeActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor monitorReg= getInstrumentation().addMonitor(WelcomeActivity2.class.getName(),null,false);
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity=mActivityTestRule.getActivity();
+    }
+    @Test
+    public void testUsernameView(){
+        View usernameView=mActivity.findViewById(R.id.username);
+        assertNotNull(usernameView);
+
+
+    }
+    @Test
+    public void TestUsernameView(){
+        View passwordView=mActivity.findViewById(R.id.password);
+        assertNotNull(passwordView);
+
+    }
+    @Test
+    public void Login(){
+        //input login details
+        onView(withId(R.id.username)).perform(typeText("tmavhona@gmail.com"));
+        //close keyboard
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("afterlife"));
+        //close keyboard
+        Espresso.closeSoftKeyboard();
+        //perform button click
+        onView(withId(R.id.login)).perform(click());
+        Activity HomeActivity =getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        assertNotNull(HomeActivity);
+        HomeActivity.finish();
+
+    }
+    @Test
+    public void RegisterPage(){
+        View Reg= mActivity.findViewById(R.id.Registration);
+        onView(withId(R.id.Registration)).perform(click());
+        Activity WelcomeActivity2 =getInstrumentation().waitForMonitorWithTimeout(monitorReg,5000);
+        assertNotNull(WelcomeActivity2);
+        WelcomeActivity2.finish();
+    }
+    @After
+    public void tearDown() throws Exception {
+        mActivity=null;
+    }
+}
