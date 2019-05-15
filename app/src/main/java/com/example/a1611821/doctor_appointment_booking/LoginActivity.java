bd2a68a9081171c863afb74dc962e917391b4699
phantom820 <1611821@students.wits.ac.za>
2019-05-15 03:00:44 +0200

    Cleared Activities

--- app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
+++ app/src/main/java/com/example/a1611821/doctor_appointment_booking/LoginActivity.java
@@ -26,57 +26,6 @@ public class LoginActivity extends AppCompatActivity {
         Username=(EditText)findViewById(R.id.username);
         Password=(EditText)findViewById(R.id.password);
 
-        Login=(Button)findViewById(R.id.login);
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String username=Username.getText().toString();
-                String password=Password.getText().toString();
-                ContentValues Params=new ContentValues();
-                Params.put("USERNAME",username);
-                Params.put("PASSWORD",password);
-
-                AsyncHTTPPost SignIn=new AsyncHTTPPost("http://lamp.ms.wits.ac.za/~s1611821/Login.php",Params) {
-                    @Override
-                    protected void onPostExecute(String output) {
-                        String form[]=output.split(",");
-                        String identity="";
-                        try {
-                            JSONArray result=new JSONArray(output);
-                            for(int i=0;i<result.length();++i){
-                                JSONObject obj=result.getJSONObject(i);
-                                identity=obj.getString("ID_NUMBER");
-                            }
-
-                            if(!identity.equals("")){
-                                Intent HomeActivity=new Intent(getApplicationContext(),HomeScreen.class);
-                                HomeActivity.putExtra("Username",username);
-                                HomeActivity.putExtra("Identity",identity);
-                                startActivity(HomeActivity);
-                            }
-                        } catch (JSONException e) {
-                             if(output.equals("unsuccessful")){
-                                Toast.makeText(getApplicationContext(),"Check your username and password",Toast.LENGTH_SHORT).show();}
-
-                              else if(output.equals("")){
-                                     Toast.makeText(getApplicationContext(),"connection error, check your internet connection",Toast.LENGTH_SHORT).show();
-                                 }
-                                  e.printStackTrace();
-
-
-
-                        }
-
-                    }
-                };
-                SignIn.execute();
-            }
-        });
     }
 
-    public void _register(View view){
-        startActivity(new Intent(getApplicationContext(), WelcomeActivity2.class));
-
-    }
-   
 }
