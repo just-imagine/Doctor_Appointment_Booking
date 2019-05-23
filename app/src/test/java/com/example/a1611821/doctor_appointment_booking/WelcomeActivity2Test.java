521bc359bc5a92d59d675a2d3861b390f7878804
rootRIDDICK <1613720@students.wits.ac.za>
2019-05-23 16:12:11 +0200

    updating welcome activity test

--- app/src/test/java/com/example/a1611821/doctor_appointment_booking/WelcomeActivity2Test.java
+++ app/src/test/java/com/example/a1611821/doctor_appointment_booking/WelcomeActivity2Test.java
@@ -1,5 +1,8 @@
 package com.example.a1611821.doctor_appointment_booking;
 
+import android.content.Intent;
+import android.view.KeyEvent;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,22 +12,43 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
+import static org.robolectric.Robolectric.buildActivity;
+
 @RunWith(RobolectricTestRunner.class)
 public class WelcomeActivity2Test {
 
     WelcomeActivity2 myReg;
+
     @Before
     public void setUp() throws Exception {
-        myReg= Robolectric.setupActivity(WelcomeActivity2.class);
+        myReg = Robolectric.setupActivity(WelcomeActivity2.class);
 
     }
 
     @After
     public void tearDown() throws Exception {
     }
+
     @Test
-    public void check(){
+    public void check() {
 
         assertNotNull(myReg);
     }
+    @Test
+    public void tryToRegister(){
+    }
+    @Test
+    public void onKeyUp_callsOnBackPressedWhichFinishesTheActivity() throws Exception {
+
+        boolean downConsumed =
+                myReg.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK));
+        boolean upConsumed =
+                myReg.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_BACK));
+
+        assertTrue(downConsumed);
+        assertTrue(upConsumed);
+        assertTrue(myReg.isFinishing());
+    }
+
+
 }
